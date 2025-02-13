3d6bffa39bd1716f44ed113fb18bffef68e49371
Samuel <sassani134@gmail.com>
2025-02-13 11:05:28 +0100

    Create a Test and it fails

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,13 +1,23 @@
 <?php
 
-namespace App\Tests;
+namespace App\Tests\Entity;
 
+use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-    public function testSomething(): void
+    public function testUserProperties(): void
     {
-        $this->assertTrue(true);
+        $user = new User();
+        
+        $user->setNom('Doe');
+        $user->setPrenom('John');
+        $user->setEmail('test@example.com');
+
+
+        $this->assertSame('Doe', $user->getNom());
+        $this->assertSame('John', $user->getPrenom());
+        $this->assertSame('test@example.com', $user->getEmail());
     }
 }
